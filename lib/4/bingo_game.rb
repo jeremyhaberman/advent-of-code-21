f1d333e48387945305b54c9608862b8b3cd465ae
Jeremy Haberman <jeremy.haberman@drip.com>
2021-12-05 10:02:17 -0600

    Day 4, parse cards

--- lib/4/bingo_game.rb
+++ lib/4/bingo_game.rb
@@ -2,12 +2,13 @@
 
 # Day 4
 class BingoGame
-  attr_reader :number_order
+  attr_reader :number_order, :cards
 
   def initialize(game_input)
     @game = game_input
     @number_order = parse_number_order(game_input)
     @number_iterator = @number_order.each
+    @cards = parse_cards(game_input)
   end
 
   def next_number
@@ -18,5 +19,12 @@ class BingoGame
 
   def parse_number_order(game_input)
     game_input.first.split(',').map(&:to_i)
+  end
+
+  def parse_cards(game_input)
+    game_input[1..]
+      .each_slice(6)
+      .map { |slice| slice[1..].map(&:strip) }
+      .map { |card| card.map { |row| row.split.map(&:to_i) } }
   end
 end
--- test/4/bingo_game_test.rb
+++ test/4/bingo_game_test.rb
@@ -15,6 +15,37 @@ class BingoGameTest < Minitest::Test
                         16, 13, 6, 15, 25, 12, 22, 18, 20, 8, 19, 3, 26, 1]
       assert_equal expected_order, @game.number_order
     end
+
+    it 'should parse three cards' do
+      assert_equal 3, @game.cards.length
+    end
+
+    it 'should parse cards' do
+      first = [
+        [22, 13, 17, 11, 0],
+        [8, 2, 23, 4, 24],
+        [21, 9, 14, 16, 7],
+        [6, 10, 3, 18, 5],
+        [1, 12, 20, 15, 19]
+      ]
+      assert_equal first, @game.cards.first
+
+      second = [
+        [3, 15, 0, 2, 22],
+        [9, 18, 13, 17, 5],
+        [19, 8, 7, 25, 23],
+        [20, 11, 10, 24, 4],
+        [14, 21, 16, 12, 6]]
+      assert_equal second, @game.cards[1]
+
+      third = [
+        [14, 21, 17, 24, 4],
+        [10, 16, 15, 9, 19],
+        [18, 8, 23, 26, 20],
+        [22, 11, 13, 6, 5],
+        [2, 0, 12, 3, 7]]
+      assert_equal third, @game.cards[2]
+    end
   end
 
   describe '#next_number' do
