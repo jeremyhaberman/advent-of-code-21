28711c7e91e3531358da9f2e639f4896bf2b51f7
Jeremy Haberman <jeremy.haberman@drip.com>
2021-12-04 18:15:32 -0600

    Refactor day 3

--- lib/3/p1/binary_diagnostic.rb
+++ lib/3/p1/binary_diagnostic.rb
@@ -4,111 +4,78 @@ require 'pry'
 
 class BinaryDiagnostic
 
-  # @param report [Array] of strings representing binary numbers
-  #   e.g. ["00100", "11110"]
+  def self.power_consumption(report)
+    self.gamma_rate(report) * self.epsilon_rate(report)
+  end
+
   def self.gamma_rate(report)
-    self.to_bits(report).
-      map { |num_as_bits| self.most_common_bit(num_as_bits) }.
+    report.first.length.times.
+      each_with_object([]) { |bit_index, array|
+        array[bit_index] = self.most_common_bit_at_index(report, bit_index)
+      }.
       reduce("") { |binary_number, bit| binary_number += bit }.
       to_i(2)
   end
 
   def self.epsilon_rate(report)
-    self.to_bits(report).
-      map { |num_as_bits| self.least_common_bit(num_as_bits) }.
+    report.first.length.times.
+      each_with_object([]) { |bit_index, array|
+        array[bit_index] = self.least_common_bit_at_index(report, bit_index)
+      }.
       reduce("") { |binary_number, bit| binary_number += bit }.
       to_i(2)
-  end
-
-  def self.power_consumption(report)
-    self.gamma_rate(report) * self.epsilon_rate(report)
-  end
-
-  def self.oxygen_generator_rating(report, important_bit: 0)
-    mcb = self.most_common_bit_at_index(report, important_bit)
-    if mcb.nil?
-      mcb = 1
-    end
-
-    matching_numbers = report.filter { |number| number[important_bit] == mcb }
-    
-    if matching_numbers.length == 1
-      matching_numbers[0].to_i(2)
-    else
-      self.oxygen_generator_rating(matching_numbers, important_bit: important_bit + 1)
-    end
-  end
-
-  def self.co2_scrubber_rating(report, important_bit: 0)
-    lcb = self.least_common_bit_at_index(report, important_bit)
-    if lcb.nil?
-      lcb = 0
-    end
-
-    matching_numbers = report.filter { |n| n[important_bit].to_i == lcb }
-    
-    if matching_numbers.length == 1
-      matching_numbers[0].to_i(2)
-    else
-      self.co2_scrubber_rating(matching_numbers, important_bit: important_bit + 1)
-    end
   end
 
   def self.life_support_rating(report)
     self.oxygen_generator_rating(report) * self.co2_scrubber_rating(report)
   end
 
-  private
-
-  def self.to_bits(report)
-    bits = report.each_with_object([]) do |number, array|
-      number.strip.split('').each_with_index do |n, i|
-        if array.at(i).nil?
-          array[i] = []
-        end
-        array[i] << n
-      end
+  def self.oxygen_generator_rating(report, important_bit: 0)
+    numbers = numbers_with_most_common_bit_at_index(report, important_bit)
+    if numbers.length == 1
+      numbers[0].to_i(2)
+    else
+      self.oxygen_generator_rating(numbers, important_bit: important_bit + 1)
     end
-    bits
   end
 
-  def self.to_first_bits(report)
-    report.map { |n| n[0] }
+  def self.co2_scrubber_rating(report, important_bit: 0)
+    numbers = numbers_with_least_common_bit_at_index(report, important_bit)
+    if numbers.length == 1
+      numbers[0].to_i(2)
+    else
+      self.co2_scrubber_rating(numbers, important_bit: important_bit + 1)
+    end
+  end
+
+  private
+
+  def self.numbers_with_most_common_bit_at_index(report, index)
+    report.filter do |number|
+      number[index] == self.most_common_bit_at_index(report, index)
+    end
+  end
+
+  def self.numbers_with_least_common_bit_at_index(report, index)
+    report.filter do |number|
+      number[index] == self.least_common_bit_at_index(report, index)
+    end
   end
 
   def self.most_common_bit_at_index(numbers, index)
-    bits_at_index = numbers.map { |n| n[index] }
-    self.most_common_bit(bits_at_index)
+    bit_counts = self.bit_counts(numbers, index)
+    bit_counts[1] >= bit_counts[0] ? "1" : "0"
   end
 
   def self.least_common_bit_at_index(numbers, index)
+    bit_counts = self.bit_counts(numbers, index)
+    bit_counts[0] <= bit_counts[1] ? "0" : "1"
+  end
+
+  def self.bit_counts(numbers, index)
     bits_at_index = numbers.map { |n| n[index].to_i }
-    bit_counts = bits_at_index.each_with_object([0, 0]) do |b, obj|
-      obj[b] += 1
+    bit_counts = bits_at_index.each_with_object([0, 0]) do |bit, array|
+      array[bit] += 1
     end
-
-    lcb = nil
-    if bit_counts[0] < bit_counts[1]
-      lcb = 0
-    elsif bit_counts[0] > bit_counts[1]
-      lcb = 1
-    end
-    lcb
-  end
-
-  def self.most_common_first_bit(bits)
-    self.most_common_bit(bits.map { |b| b[0] })
-  end
-
-  # Determines the most common bit in an array of bits
-  # @param bits [Array] of strings, each representing one bit
-  def self.most_common_bit(bits)
-    num_zeros = bits.filter { |b| b == "0" }.length
-    num_zeros > bits.length / 2.0 ? "0" : "1"
-  end
-
-  def self.least_common_bit(bits)
-    most_common_bit = self.most_common_bit(bits)
-    most_common_bit == "0" ? "1" : "0"
   end
 end
--- test/3/p1/binary_diagnostic_test.rb
+++ test/3/p1/binary_diagnostic_test.rb
@@ -6,66 +6,45 @@ require_relative '../../../lib/3/p1/binary_diagnostic'
 class BinaryDiagnosticTest < Minitest::Test
 
   def test_power_consumption
-    test_input = File.readlines('test/3/test_input.txt')
+    test_input = File.readlines('test/3/test_input.txt', chomp: true)
     power_consumption = BinaryDiagnostic.power_consumption(test_input)
     assert_equal 198, power_consumption
   end
 
   def test_gamma_rate
-    test_input = File.readlines('test/3/test_input.txt')
+    test_input = File.readlines('test/3/test_input.txt', chomp: true)
     gamma_rate = BinaryDiagnostic.gamma_rate(test_input)
     assert_equal 22, gamma_rate
   end
 
   def test_epsilon_rate
-    test_input = File.readlines('test/3/test_input.txt')
+    test_input = File.readlines('test/3/test_input.txt', chomp: true)
     gamma_rate = BinaryDiagnostic.epsilon_rate(test_input)
     assert_equal 9, gamma_rate
   end
 
   def test_most_common_bit_zero
     bits = ["0", "1", "0"]
-    mcb = BinaryDiagnostic.send(:most_common_bit, bits)
+    mcb = BinaryDiagnostic.send(:most_common_bit_at_index, bits, 0)
     assert_equal "0", mcb
   end
 
   def test_most_common_bit_one
     bits = ["0", "1", "0", "1", "1"]
-    mcb = BinaryDiagnostic.send(:most_common_bit, bits)
+    mcb = BinaryDiagnostic.send(:most_common_bit_at_index, bits, 0)
     assert_equal "1", mcb
   end
 
   def test_least_common_bit_zero
     bits = ["0", "1", "0"]
-    mcb = BinaryDiagnostic.send(:least_common_bit, bits)
+    mcb = BinaryDiagnostic.send(:least_common_bit_at_index, bits, 0)
     assert_equal "1", mcb
   end
 
   def test_least_common_bit_one
     bits = ["0", "1", "0", "1", "1"]
-    mcb = BinaryDiagnostic.send(:least_common_bit, bits)
+    mcb = BinaryDiagnostic.send(:least_common_bit_at_index, bits, 0)
     assert_equal "0", mcb
-  end
-
-  def test_to_bits
-    report = ["010", "101", "000", "111"]
-    bits = BinaryDiagnostic.send(:to_bits, report)
-    expected = [
-      ["0", "1", "0", "1"],
-      ["1", "0", "0", "1"],
-      ["0", "1", "0", "1"]
-    ]
-    assert_equal expected, bits
-  end
-
-  def test_to_bits_should_trim_whitespace
-    report = ["01\n", "10\n"]
-    bits = BinaryDiagnostic.send(:to_bits, report)
-    expected = [
-      ["0", "1"],
-      ["1", "0"]
-    ]
-    assert_equal expected, bits
   end
 
   ### PART 2 ###
@@ -88,12 +67,6 @@ class BinaryDiagnosticTest < Minitest::Test
     it "should return the CO2 scrubber rating" do
       oxygen_generator_rating = BinaryDiagnostic.co2_scrubber_rating(@test_input)
       assert_equal 10, oxygen_generator_rating
-    end
-
-    it "should return first bits" do
-      report = ["010", "101", "000", "111"]
-      first_bits = BinaryDiagnostic.send(:to_first_bits, report)
-      assert_equal ["0", "1", "0", "1"], first_bits
     end
 
     it "should return most common bit" do
